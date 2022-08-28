0fef0abb731e638e2f21b5cc19cf16b28fde49ca
Aman Shukla <amanks7400@gmail.com>
2022-08-28 11:49:54 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='p-5 flex flex-col justify-center items-center'>
+      <h1 className='font-sans-serif text-xl text-white '>
+        A Product of {' '}
+        <span className='font-bold cursor-pointer drop-shadow-sm text-heading'>
+          Rockstar Technologies
+        </span>
+      </h1>
+      <p className='font-sans-serif text-md font-semibold text-white'>
+        All rights reserved. &copy; {new Date().getFullYear()}
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
