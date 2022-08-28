05847c1e8268bd22e6cc25184bf66fc775be6851
Aman Shukla <amanks7400@gmail.com>
2022-08-28 11:51:44 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div className="h-screen w-screen bg-[url('/bg.svg')] bg-cover bg-no-repeat">
       <Appbar />
-      {children}
+      <div className="px-16">{children}</div>
       <Footer />
     </div>
   );
